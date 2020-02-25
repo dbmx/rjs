86d56e557112f7b2ca1db3cda43852eddcceb274
DBM ‪ <mpq@gmx.de>
2020-02-25 16:50:43 +0100

    Fresh Start - ReactJS

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          DBM
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://dbm.ai"
           target="_blank"
           rel="noopener noreferrer"
         >

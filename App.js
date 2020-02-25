5082618622a7b8898d9414d79f363f42aa2278a0
DBM ‪ <mpq@gmx.de>
2020-02-25 21:45:56 +0100

    Test

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function App() {
+    return(
+        <nav>Test</nav>
+    )
+
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+function MyApp() {
+    return(
+    <div>
+        <h1>Marko Simeunovic</h1>
+        <p>Biografija</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+</div>
+    )
+}
+
+
+
 // JSX
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<MyApp />, document.getElementById("root")
+
+)

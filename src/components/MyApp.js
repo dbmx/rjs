3f4b68894ceedc7869b7b14a4487744f807397c1
DBM ‪ <mpq@gmx.de>
2020-02-25 21:51:50 +0100

    Update #2

---
+++ src/components/MyApp.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyApp() {
+    return(
+    <div>
+        <h1>Marko Simeunovic</h1>
+        <p>Biografija</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+     </ul>
+    </div>
+    )
+}
+
+export default MyApp
--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
-function MyApp() {
-    return(
-    <div>
-        <h1>Marko Simeunovic</h1>
-        <p>Biografija</p>
-    <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-    </ul>
-</div>
-    )
-}
-
-
+import MyApp from "./components/MyApp"
 
 // JSX
 ReactDOM.render(<MyApp />, document.getElementById("root")
